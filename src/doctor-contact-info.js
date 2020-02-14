86a71e70f5600028b15d71f2c1c76b9a0a537aaf
Dana <dcluukko@gmail.com>
2020-02-14 15:29:06 -0800

    created doctor class and objects

---
+++ src/doctor-contact-info.js
@@ -0,0 +1,14 @@
+export class Doctor {
+  constructor(firstName, lastName, street, city, state, zip, phoneNumber, newPatient, lat, long) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.street = street;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.phoneNumber = phoneNumber;
+    this.newPatient = newPatient;
+    this.lat = lat;
+    this.long = long;
+  }
+}
