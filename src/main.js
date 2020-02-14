a11ab28a5586cd42e7d6318b2fdc96df7b2a099e
Dana <dcluukko@gmail.com>
2020-02-14 15:41:14 -0800

    attempting to configure bootstrap error

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 import $ from 'jquery';
-import 'boostrap';
+// import 'boostrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { DoctorFinder } from './doctor-search';
 
 $(document).ready(function () {
+  const symptoms = new SymptomFinder()
+  let prominse = symptoms.findSymptom()
+  promise.then(function (response) {
+    parseSymptomDate(response)
+    const finder = new DoctorFinder();
+  })
   $(".doctor-finder").submit(function (event) {
     event.preventDefault();
 
     const first = $("first-name-doc").val();
     const last = $("last-name-doc").val();
     $(".output").empty();
+
     let promise = finder.findDoctor(first, last);
   });
 });
