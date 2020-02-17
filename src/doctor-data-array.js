d87fccac93e141665adcf99f8491fe9758b021f6
Dana <dcluukko@gmail.com>
2020-02-16 17:42:26 -0800

    created doctor object that the doctor data array may be defined as

--- src/doctor-data-array.js
+++ src/doctor-data-array.js
@@ -1,8 +1,11 @@
+import { Doctor } from "../src/doctor-contact-info"
+
 export class DoctorDataArray {
   constructor() {
     this.docArray = [];
   }
-  createDocArray(doctor) {
-    this.docArray.push(doctor)
+  createDocArray(firstName, lastName, street, city, state, zip, phoneNumber, newPatient, lat, long, ) {
+    const newDoctor = new Doctor(firstName, lastName, street, city, state, zip, phoneNumber, newPatient, lat, long);
+    this.docArray.push(newDoctor)
   }
 }
