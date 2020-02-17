9d6fd17e0bcf00a1e3017306e7fa31bdec8b73f4
Dana <dcluukko@gmail.com>
2020-02-16 18:31:37 -0800

    some files not individually adding -testing push-

--- src/doctor-search.js
+++ src/doctor-search.js
@@ -29,35 +29,3 @@ export class DoctorFinder {
     }
   }
 }
-export class DoctorFinder {
-  findDoctor(firstName, lastName) {
-    return new Promise(function (resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&location=or-porland&skip=0&limit=10&user_key=${process.env.exports.apikey}';
-      request.onload = function () {
-        if (this.stats === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    })
-  }
-  findDoctorWithSymptom(symptom) {
-    return new Promise(function (resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apikey}';
-      request.onload = function () {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    })
-  }
-}
